c0e82ea9f2ff4e5f4ddfd46bf15fcc5297a6ad70
Callum Prentice <callum@gmail.com>
2021-09-10 12:05:14 -0700

    Added a Python3 tool to generate a menu with all the panos.  Also, moved over the publish scripts from publish folder into this tools one

---
+++ tools/publish.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# must run from the ./publish folder
+if [ ! -f ./publish.sh ]; then
+    echo "Run from the publish folder"
+    exit
+fi
+
+rm -rf ./site
+
+mkdir -p site/css
+cp -R ../css/* ./site/css/
+
+mkdir -p site/img
+cp -R ../img/* ./site/img/
+
+mkdir -p site/js
+cp -R ../js/* ./site/js/
+
+mkdir -p site/panos
+cp -R ../panos/* ./site/panos/
+
+cp ~/work/callum/web/web-sandbox/panos/*.jpg ./site/panos
+#menu_name=$(uuidgen).html
+menu_name=menu.html
+echo 'menu HTML is '$menu_name
+
+cp ../index.html ./site/
+cp ../menu.html ./site/
+cp ../favicon.ico ./site/
+
+cp .surgeignore ./site
+
+surge teardown https://equinaut.surge.sh
+
+curl -X POST -s --data-urlencode 'input@site/js/equinaut.js' https://javascript-minifier.com/raw > ./site/js/equinaut.min.js
+
+cp ./site/index.html ./site/index.orig
+
+sed -i -e "s/equinaut.js/equinaut.min.js/g" ./site/index.html
+
+surge --domain https://equinaut.surge.sh --project ./site
+
+cp ./site/index.orig ./site/index.html
+
+rm ./site/index.orig
+rm ./site/index.html-e
+rm ./site/js/equinaut.min.js
